6cc6b4f4508fdddc96bad02dfc299f92fa43cda2
unknown <jhenkar.xworkz@gmail.com>
2024-07-15 21:27:07 +0530

    instance variables

---
+++ Fruits.java
@@ -0,0 +1,29 @@
+public class Fruits{
+
+public Fruits(){
+	
+	
+	System.out.println("fruits details");
+	
+	
+}
+
+public String fruitname1 = "apple";
+public String fruitname2 = "greaps";
+public int appleprice = 100;
+public int  greapsprice  = 200;
+public short applekg = 10;
+public short greapkg = 20;
+public boolean apple = true;
+public boolean greaps = false;
+public long apple_load = 1000000;
+public long greap_load = 2000000;
+public float apple_loss = 123.45f;
+public float greap_loss = 4567.999f;
+public double greap_profit = 876.66666;
+public double apple_profit = 456.87776;
+public char applechar = 'a';
+public char greapchar= 'g';
+public byte appleno = 11;
+public byte greapno = 13;
+}
---
+++ FruitsRunner.java
@@ -0,0 +1,87 @@
+public class FruitsRunner{
+public static void main(String args[]){
+Fruits fruits  =   new Fruits();
+System.out.println("fruits:"+fruits.fruitname1);
+System.out.println("fruits:"+fruits.fruitname2);
+System.out.println("appleprice:"+fruits.appleprice);
+System.out.println("greapsprice:"+fruits.greapsprice);
+System.out.println("applekg:"+fruits.applekg);
+System.out.println("greapkg:"+fruits.greapkg);
+System.out.println("apple:"+fruits.apple);
+System.out.println("greap:"+fruits.greaps);
+System.out.println("apple load:"+fruits.apple_load);
+System.out.println("greap load:"+fruits.greap_load);
+System.out.println("apple loss:"+fruits.apple_loss);
+System.out.println("greap loss:"+fruits.greap_loss);
+System.out.println("apple profit:"+fruits.apple_profit);
+System.out.println("greap profit:"+fruits.greap_profit);
+System.out.println("apple:"+fruits.applechar);
+System.out.println("greap:"+fruits.greapchar);
+System.out.println("apple:"+fruits.appleno);
+System.out.println("greap:"+fruits.greapno);
+
+
+Fruits fruitsbasket = new Fruits();
+System.out.println("fruits:"+fruitsbasket.fruitname1);
+System.out.println("fruits:"+fruitsbasket.fruitname2);
+System.out.println("appleprice:"+fruitsbasket.appleprice);
+System.out.println("greapsprice:"+fruitsbasket.greapsprice);
+System.out.println("applekg:"+fruitsbasket.applekg);
+System.out.println("greapkg:"+fruitsbasket.greapkg);
+System.out.println("apple:"+fruitsbasket.apple);
+System.out.println("greap:"+fruitsbasket.greaps);
+System.out.println("apple load:"+fruitsbasket.apple_load);
+System.out.println("greap load:"+fruitsbasket.greap_load);
+System.out.println("apple loss:"+fruitsbasket.apple_loss);
+System.out.println("greap loss:"+fruitsbasket.greap_loss);
+System.out.println("apple profit:"+fruitsbasket.apple_profit);
+System.out.println("greap profit:"+fruitsbasket.greap_profit);
+System.out.println("apple:"+fruitsbasket.applechar);
+System.out.println("greap:"+fruitsbasket.greapchar);
+System.out.println("apple:"+fruitsbasket.appleno);
+System.out.println("greap:"+fruitsbasket.greapno);
+
+Fruits fruitman = new Fruits();
+System.out.println("fruits:"+fruitman.fruitname1);
+System.out.println("fruits:"+fruitman.fruitname2);
+System.out.println("appleprice:"+fruitman.appleprice);
+System.out.println("greapsprice:"+fruitman.greapsprice);
+System.out.println("applekg:"+fruitman.applekg);
+System.out.println("greapkg:"+fruitman.greapkg);
+System.out.println("apple:"+fruitman.apple);
+System.out.println("greap:"+fruitman.greaps);
+System.out.println("apple load:"+fruitman.apple_load);
+System.out.println("greap load:"+fruitman.greap_load);
+System.out.println("apple loss:"+fruitman.apple_loss);
+System.out.println("greap loss:"+fruitman.greap_loss);
+System.out.println("apple profit:"+fruitman.apple_profit);
+System.out.println("greap profit:"+fruitman.greap_profit);
+System.out.println("apple:"+fruitman.applechar);
+System.out.println("greap:"+fruitman.greapchar);
+System.out.println("apple:"+fruitman.appleno);
+System.out.println("greap:"+fruitman.greapno);
+
+
+Fruits fruitexp = new Fruits();
+System.out.println("fruits:"+fruitexp.fruitname1);
+System.out.println("fruits:"+fruitexp.fruitname2);
+System.out.println("appleprice:"+fruitexp.appleprice);
+System.out.println("greapsprice:"+fruitexp.greapsprice);
+System.out.println("applekg:"+fruitexp.applekg);
+System.out.println("greapkg:"+fruitexp.greapkg);
+System.out.println("apple:"+fruitexp.apple);
+System.out.println("greap:"+fruitexp.greaps);
+System.out.println("apple load:"+fruitexp.apple_load);
+System.out.println("greap load:"+fruitexp.greap_load);
+System.out.println("apple loss:"+fruitexp.apple_loss);
+System.out.println("greap loss:"+fruitexp.greap_loss);
+System.out.println("apple profit:"+fruitexp.apple_profit);
+System.out.println("greap profit:"+fruitexp.greap_profit);
+System.out.println("apple:"+fruitexp.applechar);
+System.out.println("greap:"+fruitexp.greapchar);
+System.out.println("apple:"+fruitexp.appleno);
+System.out.println("greap:"+fruitexp.greapno);
+
+
+}
+}
