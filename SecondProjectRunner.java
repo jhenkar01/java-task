0731b431184b0f70088ff02122b23dcc637ef054
unknown <jhenkar.xworkz@gmail.com>
2024-08-09 15:30:48 +0530

    CRUD operation ex

---
+++ SecondProjectRunner.java
@@ -0,0 +1,32 @@
+package com.xworkz.secondproject.runner;
+
+import com.xworkz.secondproject.things.SecondProject;
+
+public class SecondProjectRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SecondProject fruits = new SecondProject();
+		int aboutFruits = fruits.aboutFruits();
+	System.out.println("the array length is:"+aboutFruits);
+     fruits.fruitsinformation("mango");
+	      fruits.fruitsinformation("apple");
+		  fruits.fruitsDetails();
+		  System.out.println("------------updateFruits---------");
+		  String updatefruits=fruits.updateFruits("orange","mango");
+		  System.out.println("updated fruit:"+updatefruits);
+		  fruits.fruitsDetails();
+		  System.out.println("------------deletedFruits---------");
+		  String deletefruits=fruits.deleteFruits("apple");
+		  System.out.println("deleted fruit:"+deletefruits);
+	  	 fruits.fruitsDetails();
+		 System.out.println("------------searchedFruits---------");
+         String searchfruits=fruits.searchFruits("orange");
+ 		 System.out.println("searching:"+searchfruits);
+	  	 fruits.fruitsDetails();  
+}
+
+	}
+
+
